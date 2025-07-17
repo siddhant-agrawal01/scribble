1f24caf24c97d42d174d374e2b5289e5880694fe
siddhant agrawal <siddhant-agrawal01>
2025-07-17 21:10:23 +0530

    implemented common conf for jwt secret

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -1,13 +1,13 @@
 import express from "express";
 import jwt from "jsonwebtoken"
-import { JWT_SECRET } from "./config";
+import { JWT_SECRET } from "@repo/backend-common/config";
 import { middleware } from "./middleware";
 
 const app = express();
 
 
 app.post("/signin",(req,res)=>{
-
+JWT_SECRET
 })
 
 app.post("/signup",(req,res)=>{
